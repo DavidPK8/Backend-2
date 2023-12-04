b67c69ba697a2d4e83f19f8ff5911348345dddb0
DavidPK8 <alejodavicho8@gmail.com>
2023-12-04 15:55:21 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -6,15 +6,73 @@ const express = require('express')
 // Crear una instancia
 const app = express()
 
+// Mandar texto
+app.get('/hamburguesa/simple', (req, res) =>{
+    res.send("Hamburguesa - simple")
+})
+
+console.log(__dirname)
+
+// Mandar imagenes
+app.get('/hamburguesa/doble', (req, res) =>{
+    res.sendFile('./doble.jpg',{
+        root:__dirname
+    })
+})
+
+// Mandar documento de word
+app.get('/hamburguesa/triple', (req, res) =>{
+    res.sendFile('./triple.docx',{
+        root:__dirname
+    })
+})
+
+// Manda un json
+app.get('/hamburguesa/mixta', (req, res) =>{
+    res.status(200).json({
+        "tipo": "Mixta",
+        "extra": "No"
+    })
+})
+
+
+/*
+USO DE QUERY PARAMS
+app.get('/search', (req, res) =>{
+    // console.log(req.query) Lo que se ingrese en google se imprimira en la terminal
+    if(req.query.tipo == "sencilla"){
+        res.send("Hamburguesa sencilla")
+    }
+    else{
+        res.send("No existe resultados")
+    }
+})*/
+
+/*
+USO DE PARAMS
+// Mandar info en formato JSON
+app.use(express.json())
+
+app.get('/pedido/:tipo', (req, res) =>{
+    // console.log(req.params) Lo que se ingrese en google se imprimira en la terminal
+    res.send(`El pedido es hamburguesa ${req.params.tipo}`)
+})*/
+
+
+/*
+MANDAR INFO EN JSON
 // Mandar info en formato JSON
 app.use(express.json())
 
 // Se crea una ruta por Post
 app.post('/register', (req, res) => {
-    console.log(req.body)
-})
+    const{email, password} = req.body // Destructuracion de JSv
+    res.send(`El email es ${email} y su password es ${password}`)  // Backlick
+})*/
+
 
 /*
+MANDAR INFO A UN LOCALHOST
 // Crear la ruta
 app.get('/', (req, res) =>{
     res.send("Bienvenidos")
